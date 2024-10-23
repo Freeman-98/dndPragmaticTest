abf115ad77e2deec895743cff641c4cd7963c75c
Freeman-98 <marianocornejomariano@gmail.com>
2024-10-23 13:33:35 -0300

    algo

--- src/assets/IMG_UTILS.js
+++
@@ -1,7 +0,0 @@
-import king from '../src/assets/king.png';
-import pawn from './icons/pawn.png';
-
-export const IMG_UTILS = [
-    {K:king},
-    {P:pawn}
-]
---
+++ src/assets/imUtils.ts
@@ -0,0 +1,7 @@
+import King from './king.png';
+import Pawn from './pawn.png';
+
+export const IMG_UTILS = {
+    king: King,
+    pawn: Pawn
+}
